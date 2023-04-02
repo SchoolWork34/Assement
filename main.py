d00e13913525154b74dac3c4455c4fdfbe9bf2c7
AJ Zeelie <127257818+SchoolWork34@users.noreply.github.com>
2023-04-02 19:09:00 +0000

    doing better

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
 correct = 0
-def correct(correct):
-  if Answers_0 == 34:
-    correct+=1
-  if Answers_1 == 18:
-    correct+=1
-  if Answers_2 == -99:
-    correct+=1
-  if Answers_3 == 3.75:
-    correct+=1
-  if Answers_4 == 227:
-    correct+=1
-  if Answers_5 == 50:
-    correct+=1
-  if Answers_6 == 178:
-    correct+=1
-  if Answers_7 == 2.71:
-    correct+=1
-  if Answers_8 == 49:
-    correct+=1
-  if Answers_9 == -140:
-    correct+=1
+def CorrectTest(correct):
+  if Answers_0 == Answers:
+    correct + 1
 
-def percentage():
+def PercentageTest(correct):
   if correct == 1:
     return(userName + "you got 10% correct")
   if correct == 2:
@@ -43,54 +25,29 @@ def percentage():
   if correct == 10:
     return(userName + "you got 100% correct")
 
-
 #Says welcome to this BEDMAS quiz
 print("Welcome to this BEDMAS quiz.")
 #Says do not use a calculator
 print("Do not use a calculator")
-
 #Asks for their name
 userName = input("Name please ")
-
 #Says lets get started with what ever their name is
 print("Lets get started " + userName)
-
-#Says what the math methods are 
+#Says what the math methods are
 print("/ = divide, * = multiply, - = subtract, + = add")
-
 #Says here are the questions
 print("Here are the questions")
-
 current_Question = 0
 #The question list
 Question = ['4+5(2*3) = ','5-2+3*5 = ','2/2-1*100 = ','2+2-2*2/2(2*4) = ','15/3*5(2+3+4) = ','7*14/2+1 = ','2*2*2*22*1+2 = ','3+4-5*6/7 = ','10/1*3+5-7(9-11) = ','10-10*15 = ']
-
 #Answers list
 Answers = ['34','18','-99','3.75','227','50','178','2.71','49','-140']
-
 #prints the questions to the screen
 for x in Question:
   Answers_0 = input(Question[current_Question])
   current_Question+=1
-  Answers_1 = input(Question[current_Question])
-  current_Question+=1
-  Answers_2 = input(Question[current_Question])
-  current_Question+=1
-  Answers_3 = input(Question[current_Question])
-  current_Question+=1
-  Answers_4 = input(Question[current_Question])
-  current_Question+=1
-  Answers_5 = input(Question[current_Question])
-  current_Question+=1
-  Answers_6 = input(Question[current_Question])
-  current_Question+=1
-  Answers_7 = input(Question[current_Question])
-  current_Question+=1
-  Answers_8 = input(Question[current_Question])
-  current_Question+=1
-  Answers_9 = input(Question[current_Question])
-  current_Question+=1
-  Peace = input("How do you think you did out of 10? ")
+ 
 
-print(correct)
-print(percentage)
+print(CorrectTest(correct))
+
+print(PercentageTest(correct))
